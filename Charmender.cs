1ceceae30bc85ec9803495bd51de396f09e58d38
EnesT51 <99040669@mydavinci.nl>
2023-05-11 16:15:34 +0200

    creaded a .cs file for Charmender to make it more readeble

---
+++ Charmender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemen_Battle_Simulator
+{
+    class Charmender
+    {
+        public string Name { get; set; }
+        public string Strength { get; set; }
+        public string Weakness { get; set; }
+
+        public Charmender(string name, string strength, string weakness)
+        {
+            Name = name;
+            Strength = strength;
+            Weakness = weakness;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public void Setname(string name)
+        {
+            Name = name;
+        }
+        public void GetStrength(string strength)
+        {
+            Strength = strength;
+        }
+        public string strength()
+        {
+            return Strength;
+        }
+        public void weakness(string weakness)
+        {
+            Weakness = weakness;
+        }
+        public string ReturnWeakness()
+        {
+            return Weakness;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
-namespace BattleSimulator;
+namespace Pokemen_Battle_Simulator;
 
 class Program
 {
@@ -26,42 +25,4 @@ class Program
     }
 }
 
-class Charmender
-{
-    public string Name { get; set; }
-    public string Strength { get; set; }
-    public string Weakness { get; set; }
 
-    public Charmender(string name, string strength, string weakness)
-    {
-        Name = name;
-        Strength = strength;
-        Weakness = weakness;
-    }
-
-    public string GetName()
-    {
-        return Name;
-    }
-    public void Setname(string name)
-    {
-        Name = name;
-    }
-    public void GetStrength(string strength)
-    {
-        Strength = strength;
-    }
-    public string strength()
-    {
-        return Strength;
-    }
-    public void weakness(string weakness)
-    {
-        Weakness = weakness;
-    }
-    public string ReturnWeakness()
-    {
-        return Weakness;
-    }
-
-}
