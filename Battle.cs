39afa2988155672155fc37c10a43e9d0ef9d5d54
EnesT51 <99040669@mydavinci.nl>
2023-06-04 22:48:06 +0200

    added battle and arena class

---
+++ Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemen_Battle_Simulator
+{
+    public class Battle
+    {
+        public static void PokemonBattle(Trainer Player1, Trainer Player2)
+        {
+
+        }
+    }
+}
